b48232735adceb30bd1214cb2b604b2213683b76
fayezosaadi <26156279+fayezosaadi@users.noreply.github.com>
2026-08-15 11:38:07 -0600

    add oidc

--- modules/github_actions_oidc/data.tf
+++ modules/github_actions_oidc/data.tf
@@ -1,5 +1,9 @@
-data "azuread_application_published_app_ids" "well_known" {}
+data "azuread_application_published_app_ids" "well_known" {
+  count = var.grant_msgraph_readonly_application_permissions || var.grant_msgraph_readwrite_application_permissions ? 1 : 0
+}
 
 data "azuread_service_principal" "msgraph" {
-  client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+  count = var.grant_msgraph_readonly_application_permissions || var.grant_msgraph_readwrite_application_permissions ? 1 : 0
+
+  client_id = data.azuread_application_published_app_ids.well_known[0].result["MicrosoftGraph"]
 }
